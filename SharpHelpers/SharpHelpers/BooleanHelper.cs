14b785ae86b6a47b5f52162b092fe8e0a284e3be
matpresidentric <mat.president.ric@hotmail.it>
2019-07-19 15:21:37 +0200

    Update

--- SharpHelpers/SharpHelpers/BooleanHelper.cs
+++ SharpHelpers/SharpHelpers/BooleanHelper.cs
@@ -40,5 +40,21 @@ namespace SharpCoding.SharpHelpers
         {
             return istance || op2;
         }
+        
+        public static string ToSI_NO(this bool? value)
+        {
+            return value.HasValue ? (value.Value ? "SI" : "NO") : "NO";
+        }
+
+        public static string ToS_N(this bool? value, string @default = "N")
+        {
+            return value.HasValue ? (value.Value ? "S" : "N") : @default;
+        }
+
+        public static bool BoolFromString(this string value)
+        {
+            return value != null &&
+                (value.Equals("s", StringComparison.InvariantCultureIgnoreCase) || value.Equals("si", StringComparison.InvariantCultureIgnoreCase));
+        }
     }
 }
