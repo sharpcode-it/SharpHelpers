3d79008a55cbb300b1814bd3412f4986ca28953f
Fabio Mannis <f.mannis@almaviva.it>
2019-07-21 16:48:30 +0200

    New method on stringhelper : SqlInjectionSanitize

--- SharpHelpers/SharpHelpers/StringHelper.cs
+++ SharpHelpers/SharpHelpers/StringHelper.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace SharpCoding.SharpHelpers
 {
@@ -12,6 +10,28 @@ namespace SharpCoding.SharpHelpers
             return string.IsNullOrEmpty(istance)
                 ? string.Empty
                 : toReplace.Aggregate(istance, (current, str) => current.Replace(str, replaceWith));
+        }
+
+        public static string SqlInjectionSanitize(this string stringValue)
+        {
+            if (null == stringValue)
+                return string.Empty;
+            return stringValue.RegexReplace("-{2,}", "-") // transforms multiple --- in - use to comment in sql scripts
+                .RegexReplace(@"[*/]+", string.Empty) // removes / and * used also to comment in sql scripts
+                .RegexReplace(
+                    @"(;|\s)(exec|execute|select|insert|update|delete|create|alter|drop|rename|truncate|backup|restore)\s",
+                    string.Empty, RegexOptions.IgnoreCase).Trim();
+        }
+
+        
+        private static string RegexReplace(this string stringValue, string matchPattern, string toReplaceWith)
+        {
+            return Regex.Replace(stringValue, matchPattern, toReplaceWith);
+        }
+
+        private static string RegexReplace(this string stringValue, string matchPattern, string toReplaceWith, RegexOptions regexOptions)
+        {
+            return Regex.Replace(stringValue, matchPattern, toReplaceWith, regexOptions);
         }
     }
 }
