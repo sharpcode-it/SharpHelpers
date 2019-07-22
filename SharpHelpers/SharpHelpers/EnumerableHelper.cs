1712b078da2e281873989febebdf7c6e2fcced68
VforValerioB <valerio.benz@gmail.com>
2019-07-22 10:04:14 +0200

    Added EnumerableHelper

---
+++ SharpHelpers/SharpHelpers/EnumerableHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpCoding.SharpHelpers
+{
+    public static class EnumerableHelper
+    {
+        /// <summary>
+        /// This method returns a sublist of the instance after a distinctby operation
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="propertySelector"></param>
+        /// <returns></returns>
+        public static IEnumerable DistinctBy<T>(this List<T> list, Func<T, object> propertySelector)
+        {
+             return list.GroupBy(propertySelector).Select(x => x.First());
+        }
+    }
+}
