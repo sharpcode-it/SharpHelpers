1a208f3dc81601e458036f523349479fa7688f12
ivano scifoni <iscifoni@libero.it>
2019-09-05 17:53:23 +0200

    Update EnumerableHelper.cs
    license on top

--- SharpHelpers/SharpHelpers/EnumerableHelper.cs
+++ SharpHelpers/SharpHelpers/EnumerableHelper.cs
@@ -1,11 +1,11 @@
-﻿using System;
+
+// (c) 2019 SharpCoding
+// This code is licensed under MIT license (see LICENSE.txt for details)using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace SharpCoding.SharpHelpers
 {
-    // (c) 2019 SharpCoding
-    // This code is licensed under MIT license (see LICENSE.txt for details)
     public static class EnumerableHelper
     {
         /// <summary>
