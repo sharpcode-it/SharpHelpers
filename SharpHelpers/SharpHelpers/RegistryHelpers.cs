967f15eafdfe33e6d565a6d6e74fe2314d1e8bf6
ivano scifoni <iscifoni@libero.it>
2019-09-05 17:55:24 +0200

    Update RegistryHelpers.cs

--- SharpHelpers/SharpHelpers/RegistryHelpers.cs
+++ SharpHelpers/SharpHelpers/RegistryHelpers.cs
@@ -1,11 +1,12 @@
-﻿using System;
+// (c) 2019 SharpCoding
+// This code is licensed under MIT license (see LICENSE.txt for details)
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SharpCoding.SharpHelpers
 {
-    // (c) 2019 SharpCoding
-    // This code is licensed under MIT license (see LICENSE.txt for details)
+
     class RegistryHelpers
     {
     }
