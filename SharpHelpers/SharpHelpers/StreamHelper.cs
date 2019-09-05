1c550088d10b7f1f7d00053b5b2e70ee5172e46a
ivano scifoni <iscifoni@libero.it>
2019-09-05 17:56:04 +0200

    Update StreamHelper.cs

--- SharpHelpers/SharpHelpers/StreamHelper.cs
+++ SharpHelpers/SharpHelpers/StreamHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+// (c) 2019 SharpCoding
+// This code is licensed under MIT license (see LICENSE.txt for details)
+using System;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -6,8 +8,6 @@ using System.Text;
 
 namespace SharpCoding.SharpHelpers
 {
-    // (c) 2019 SharpCoding
-    // This code is licensed under MIT license (see LICENSE.txt for details)
     public static class StreamHelper
     {
         /// <summary>
