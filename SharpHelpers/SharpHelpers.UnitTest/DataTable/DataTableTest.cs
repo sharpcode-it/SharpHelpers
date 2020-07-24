0ee85920644359128ff70ef9f1ff6ffaa177b9c6
engineering87 <francesco.delre.87@gmail.com>
2020-07-24 23:55:18 +0200

    Add DataTable helper

---
+++ SharpHelpers/SharpHelpers.UnitTest/DataTable/DataTableTest.cs
@@ -0,0 +1,27 @@
+﻿// (c) 2020 SharpCoding
+// This code is licensed under MIT license (see LICENSE.txt for details)
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpCoding.SharpHelpers;
+
+namespace SharpHelpers.UnitTest.DataTable
+{
+    [TestClass]
+
+    public class DataTableTest
+    {
+        [TestMethod]
+        public void TestColumnsOrder()
+        {
+            var dt = new System.Data.DataTable();
+            dt.Columns.Add("First", typeof(string));
+            dt.Columns.Add("Second", typeof(string));
+
+            var columnOrder = new string[] { "Second", "First" };
+
+            dt.SetColumnsOrder(columnOrder);
+
+            Assert.IsTrue(dt.Columns[0].ColumnName == columnOrder[0]);
+            Assert.IsTrue(dt.Columns[1].ColumnName == columnOrder[1]);
+        }
+    }
+}
---
+++ SharpHelpers/SharpHelpers/DataTableHelper.cs
@@ -0,0 +1,29 @@
+﻿// (c) 2020 SharpCoding
+// This code is licensed under MIT license (see LICENSE.txt for details)
+using System.Data;
+
+namespace SharpCoding.SharpHelpers
+{
+    public static class DataTableHelper
+    {
+        /// <summary>
+        /// This method set the columns order by name
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="columnNames"></param>
+        /// <returns></returns>
+        public static DataTable SetColumnsOrder(this DataTable table, string[] columnNames)
+        {
+            int columnIndex = 0;
+            foreach (var columnName in columnNames)
+            {
+                if (table.Columns.Contains(columnName))
+                {
+                    table.Columns[columnName].SetOrdinal(columnIndex);
+                    columnIndex++;
+                }
+            }
+            return table;
+        }
+    }
+}
