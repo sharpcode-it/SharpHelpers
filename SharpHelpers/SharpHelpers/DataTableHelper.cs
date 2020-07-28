3a76b13287ded4fe9d5960506110c47960345e0a
engineering87 <francesco.delre.87@gmail.com>
2020-07-28 02:39:54 +0200

    Add ToList in DataTableHelper

--- SharpHelpers/SharpHelpers/DataTableHelper.cs
+++ SharpHelpers/SharpHelpers/DataTableHelper.cs
@@ -1,6 +1,9 @@
 ﻿// (c) 2020 SharpCoding
 // This code is licensed under MIT license (see LICENSE.txt for details)
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Reflection;
 
 namespace SharpCoding.SharpHelpers
 {
@@ -24,6 +27,30 @@ namespace SharpCoding.SharpHelpers
                 }
             }
             return table;
+        }
+
+        /// <summary>
+        /// This method return a list of objects
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public static List<T> ToList<T>(this DataTable table) where T : new()
+        {
+            var list = new List<T>();
+            foreach (DataRow row in table.Rows)
+            {
+                var objClass = new T();
+                var type = objClass.GetType();
+                foreach (DataColumn column in row.Table.Columns)
+                {
+                    var prop = type.GetProperty(column.ColumnName);
+                    if (prop != null)
+                        prop.SetValue(objClass, row[column.ColumnName], null);
+                }
+                list.Add(objClass);
+            }
+            return list;
         }
     }
 }
