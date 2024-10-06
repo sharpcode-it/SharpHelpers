679ce24fe2aa9aa5a8b35fb928c258067838b84e
Francesco Del Re <francesco.delre.87@gmail.com>
2024-10-06 16:28:41 +0200

    Refactor IsSystemType

--- SharpHelpers/SharpHelpers/ObjectHelper.cs
+++ SharpHelpers/SharpHelpers/ObjectHelper.cs
@@ -7,29 +7,37 @@ namespace SharpCoding.SharpHelpers
 {
     public static class ObjectHelper
     {
+        private static readonly Type[] SystemTypes =
+        {
+            typeof(Enum),
+            typeof(string),
+            typeof(decimal),
+            typeof(DateTime),
+            typeof(DateTimeOffset),
+            typeof(TimeSpan),
+            typeof(Guid)
+        };
+
         /// <summary>
-        ///  This method verifies that the passed type is a .Net Type 
+        /// Checks if the given type is a system type (e.g., primitive, value types, common system types).
         /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
+        /// <param name="type">The type to check.</param>
+        /// <returns>True if the type is a system type; otherwise, false.</returns>
         public static bool IsSystemType(this Type type)
         {
-            return
-                type.IsValueType ||
-                type.IsPrimitive ||
-                new[]
-                {
-                    typeof(Enum),
-                    typeof(string),
-                    typeof(decimal),
-                    typeof(DateTime),
-                    typeof(DateTimeOffset),
-                    typeof(TimeSpan),
-                    typeof(Guid)
-                }.Contains(type) ||
-                Convert.GetTypeCode(type) != TypeCode.Object ||
-                (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>) &&
-                 IsSystemType(type.GetGenericArguments()[0]));
+            if (type.IsValueType || type.IsPrimitive)
+                return true;
+
+            // Handle Nullable types
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+                return IsSystemType(type.GetGenericArguments()[0]);
+
+            // Check against predefined system types
+            if (SystemTypes.Contains(type))
+                return true;
+
+            // Check the type code to cover other system types
+            return Convert.GetTypeCode(type) != TypeCode.Object;
         }
     }
 }
