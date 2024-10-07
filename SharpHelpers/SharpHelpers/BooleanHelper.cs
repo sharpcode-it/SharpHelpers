d8d70e6cecba2d3fa6d0789429f6cc9205523716
Francesco Del Re <francesco.delre.87@gmail.com>
2024-10-08 00:39:03 +0200

    Add boolean methods

--- SharpHelpers/SharpHelpers/BooleanHelper.cs
+++ SharpHelpers/SharpHelpers/BooleanHelper.cs
@@ -1,5 +1,7 @@
 ﻿// (c) 2019 SharpCoding
 // This code is licensed under MIT license (see LICENSE.txt for details)
+using System;
+
 namespace SharpCoding.SharpHelpers
 {
     public static class BooleanHelper
@@ -48,6 +50,52 @@ namespace SharpCoding.SharpHelpers
         public static string ToStringValues(this bool? value, string trueValue, string falseValue)
         {
             return value.HasValue ? (value.Value ? trueValue : falseValue) : falseValue;
+        }
+
+        /// <summary>
+        /// Negates the instance boolean value.
+        /// </summary>
+        /// <param name="instance">The boolean value to negate.</param>
+        /// <returns>Returns the negated boolean value.</returns>
+        public static bool Not(this bool instance)
+        {
+            return !instance;
+        }
+
+        /// <summary>
+        /// Determines if the instance is true and executes the specified action if true.
+        /// </summary>
+        /// <param name="instance">The boolean value to evaluate.</param>
+        /// <param name="action">The action to execute if the boolean value is true.</param>
+        public static void IfTrue(this bool instance, Action action)
+        {
+            if (instance)
+            {
+                action?.Invoke();
+            }
+        }
+
+        /// <summary>
+        /// Determines if the instance is false and executes the specified action if false.
+        /// </summary>
+        /// <param name="instance">The boolean value to evaluate.</param>
+        /// <param name="action">The action to execute if the boolean value is false.</param>
+        public static void IfFalse(this bool instance, Action action)
+        {
+            if (!instance)
+            {
+                action?.Invoke();
+            }
+        }
+
+        /// <summary>
+        /// Returns the boolean value as an integer (1 for true, 0 for false).
+        /// </summary>
+        /// <param name="instance">The boolean value to convert.</param>
+        /// <returns>1 if true, 0 if false.</returns>
+        public static int ToInt(this bool instance)
+        {
+            return instance ? 1 : 0;
         }
     }
 }
